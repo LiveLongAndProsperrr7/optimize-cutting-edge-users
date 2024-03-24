4f93f631f95dfa6967245c7b8a18a3679856ea5e
LiveLongAndProsperrr7 <157714423+LiveLongAndProsperrr7@users.noreply.github.com>
2024-03-24 10:29:24 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+
+        }
+        int[] data = new int[10];
+}
+
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        for (int item : data) {
+        }
